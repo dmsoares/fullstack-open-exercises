02ae67480b1f2899df52a811b5ddd51bd1b2507c
Decio <decio.msoares@gmail.com>
2020-09-16 17:06:03 +0100

    add Part component for 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,11 +7,15 @@ const Header = (props) => (
   </div>
 )
 
+const Part = (props) => (
+  <p>{props.part} {props.exercise}</p>
+)
+
 const Content = (props) => (
   <div>
-    <p>{props.part1} {props.exercise1}</p>
-    <p>{props.part2} {props.exercise2}</p>
-    <p>{props.part3} {props.exercise3}</p>
+    <Part part={props.part1} exercise={props.exercise1} />
+    <Part part={props.part2} exercise={props.exercise2}/>
+    <Part part={props.part3} exercise={props.exercise3}/>
   </div>
 )
 
