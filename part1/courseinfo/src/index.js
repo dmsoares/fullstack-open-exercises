c6d507b1b58e60ae3e984d717a87de2c1089e50a
Decio <decio.msoares@gmail.com>
2020-09-16 15:48:37 +0100

    change props name

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -9,7 +9,7 @@ const Header = (props) => (
 
 const Content = (props) => (
   <div>
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.part} {props.exercise}</p>
   </div>
 )
 
@@ -32,9 +32,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
       <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
