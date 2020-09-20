f37c2c396dded0c73fd113f35dae4c0b13a44863
Decio <decio.msoares@gmail.com>
2020-09-20 23:48:24 +0100

    add 2.5 solution

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,42 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((acc, part) => acc += part.exercises, 0);
-  return(
-    <p><strong>total of {sum} exercises</strong></p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Course = ({course}) => (
-  <>
-    <Header course={course} />
-    <Content course={course} />
-    <Total course={course} />
-  </>
-)
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
