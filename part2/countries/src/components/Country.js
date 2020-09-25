e1f5059a9eaaa16833e063a7461b48d418ac4588
Decio <decio.msoares@gmail.com>
2020-09-26 00:01:08 +0100

    add 2.14 solution

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const api_key = process.env.REACT_APP_API_KEY
 
 const Country = ({match}) => {
+    const [weather, setWeather] = useState()
+    const api_key = '81fa97d074b327564b4e2aec66db9c58'
+    useEffect(() => {
+        axios
+            .get(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`)
+            .then(response => setWeather(response.data))
+    }, [match])
     return (
         <>
             <h1>{match.name}</h1>
@@ -11,6 +21,13 @@ const Country = ({match}) => {
                 {match.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}
             </ul>
             <img src={match.flag} alt={`The flag of ${match.name}`} style={{width: '100px'}}></img>
+            {weather ? (
+                <section>
+                    <h2>Weather in {match.capital}</h2>
+                    <p><strong>temperature:</strong> {weather.main.temp}</p>
+                    <p><strong>wind</strong> speed: {weather.wind.speed} deg: {weather.wind.deg}</p>
+                </section>)
+                : null}
         </>
     )
 }
