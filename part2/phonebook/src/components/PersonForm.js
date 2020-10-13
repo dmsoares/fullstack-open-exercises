edf16f04140754906c094b4bbdbe5b697155a00c
Decio <decio.msoares@gmail.com>
2020-10-13 01:59:25 +0100

    tidy up handler in PersonForm

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
 const PersonForm = ({handleOnSubmit, handleName, handleNumber, name, number}) => {
-    return (
-      <form onSubmit={(event) => handleOnSubmit(event)}>
-        <div>
-          name: <input id='name-input' onChange={(event) => handleName(event.target.value)} value={name} autoFocus/>
-        </div>
-        <div>
-          number: <input onChange={(event) => !/[^\d\-\s]|\s\s+|--+|^-|^\s|\s-+|-\s+/g.test(event.target.value) && handleNumber(event.target.value)} value={number} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-    )
+
+  const handleNumberChange = (event) => !/[^\d\-\s]|\s\s+|--+|^-|^\s|\s-+|-\s+/g.test(event.target.value) && handleNumber(event.target.value) 
+
+  return (
+    <form onSubmit={(event) => handleOnSubmit(event)}>
+      <div>
+        name: <input id='name-input' onChange={(event) => handleName(event.target.value)} value={name} autoFocus />
+      </div>
+      <div>
+        number: <input onChange={handleNumberChange} value={number} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
 }
 
 export default PersonForm;
